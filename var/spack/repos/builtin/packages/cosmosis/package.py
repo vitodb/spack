f8ef43db4b9ccfad7533acbd7457f0a82209b3ec
Vito Di Benedetto <vito@fnal.gov>
2020-04-06 16:10:40 -0500

    cleanup CosmoSIS recipe and included package description provided by Marc P

--- var/spack/repos/builtin/packages/cosmosis/package.py
+++ var/spack/repos/builtin/packages/cosmosis/package.py
@@ -3,31 +3,13 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install cosmosis
-#
-# You can edit this file again by typing:
-#
-#     spack edit cosmosis
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 import os
 
 class Cosmosis(MakefilePackage):
-    """FIXME: Put a proper description of your package here."""
+    """CosmoSIS is a modular system for cosmological parameter estimation problems."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://www.example.com"
+    homepage = "https://bitbucket.org/joezuntz/cosmosis/wiki/Home"
     url      = "https://bitbucket.org/joezuntz/cosmosis/get/v1.6.2.tar.bz2"
     git      = 'https://bitbucket.org/joezuntz/cosmosis.git'
 
@@ -51,15 +33,6 @@ class Cosmosis(MakefilePackage):
              commit="30e90c9e8882aa6505e2019ad8b6ef4196471109",
              destination='.', when='@neutrinoless_mass_function_2')
 
-    # FIXME: Add dependencies if required.
-    # depends_on('foo')
-    #depends_on('python@3.8.1:')
-    #depends_on('gcc@8.3.0:')
-
-    #cosmosis-standard-library
-    #depends_on('cosmosis-standard-library@neutrinoless_mass_function_1', when='@neutrinoless_mass_function_1')
-    #depends_on('cosmosis-standard-library')
-
     depends_on('py-configparser')
     depends_on('py-future')
     depends_on('py-ipython')
@@ -80,11 +53,6 @@ class Cosmosis(MakefilePackage):
     depends_on('minuit')
     depends_on('sqlite')
     depends_on('cfitsio')
-    #depends_on('')
-
-
-    #phases = ['build', 'install']
-    #phases = ['build']
 
     def setup_build_environment(self, env):
         """Set up the build environment for this package."""
@@ -97,12 +65,6 @@ class Cosmosis(MakefilePackage):
         env.set('CFITSIO_INC', self.spec['cfitsio'].prefix+'/include')
         env.set('CFITSIO_LIB', self.spec['cfitsio'].prefix+'/lib')
 
-    #def build(self, spec, prefix):
-    #    os.symlink(self.spec['cosmosis-standard-library'].prefix, self.build_directory+'/cosmosis-standard-library')
-    #    make()
-
-    #def install(self, spec, prefix):
-    #    install_tree('.', prefix)
 
     def build(self, spec, prefix):
         import inspect
@@ -124,8 +86,3 @@ class Cosmosis(MakefilePackage):
         env.prepend_path('LD_LIBRARY_PATH',self.prefix+'/cosmosis-standard-library/likelihood/planck2015/plc-2.0/lib')
         env.set('COSMOSIS_SRC_DIR', self.prefix)
 
-#    def edit(self, spec, prefix):
-        # FIXME: Edit the Makefile if necessary
-        # FIXME: If not needed delete this function
-        # makefile = FileFilter('Makefile')
-        # makefile.filter('CC = .*', 'CC = cc')
